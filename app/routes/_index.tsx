4964abb400aaaf9255a5d8554882f6a693cb012d
dorothy <dorothy.x.ren@gmail.com>
2024-06-07 19:26:30 -0400

    first attempt at creating new survey, doesn't correctly load into database

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,7 +1,11 @@
 import { PrismaClient } from "@prisma/client";
-import type { MetaFunction } from "@remix-run/node";
-import { Form, useLoaderData } from "@remix-run/react";
+import type { ActionFunction, MetaFunction } from "@remix-run/node";
+import { Form, useLoaderData, Link } from "@remix-run/react";
 import client from "~/client";
+
+//create this variable. it is empty for now bc the input field will fill in.
+let surveyName = ""
+let QuestionOne = ""
 
 export const loader = async () => {
   const surveys = await client.survey.findMany();
@@ -22,6 +26,16 @@ export const loader = async () => {
 //   //return a response
 // }
 
+//Remix takes the action and turns it into a PUT request. Anything that isn't a GET request. GET is the default export of
+// component. If the component isn't there, it's the loader
+//one action per route
+//actions define POST - define how the post/put is handled. anything not a get
+// loaders define how data is loaded
+//components are returned on a GET
+export const action: ActionFunction = async ({ }) => {
+  return new Response("this is a test action survey");
+}
+
 
 export default function Index() {
   const data = useLoaderData<typeof loader>();
@@ -33,21 +47,22 @@ export default function Index() {
       <h1>Survey List</h1>
     </div><ul>
         {surveys.map((element, index, array) => {
-          const survey = element
-          console.log(survey)
+          const survey = element;
+          console.log(survey);
           return (
             <>
               <div className="flex">
                 <li key={survey.id}>{survey.title}</li>
-                <button className="bg-green-500 border border-gray-500 flex flex-row"> Take this survey </button>
-              </div>
+                <button className="bg-purple-200 border border-gray-500 flex flex-row rounded-md font-bold py-2 px-4"> Take this survey </button>
 
+              </div>
+              <div className="bg-green-200 border border-gray-500 flex flex-row rounded-md font-bold py-2 px-4 w-60">
+                <Link to="/create"> New Survey </Link>
+              </div>
             </>
-          )
+          );
         }
         )}
       </ul></>
-  );
-};
-
-
+  )
+}
